f2b0dcd2eb9c89d8b92e27359eda809a6121c0e4
Nursena Kal <nursena.kal@ug.bilkent.edu.tr>
2018-12-22 01:00:37 +0300

    yeni bişeyler

--- app/src/main/java/coco/coconutreserve/Dashboard.java
+++ app/src/main/java/coco/coconutreserve/Dashboard.java
@@ -5,11 +5,35 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import coco.coconutreserve.assets.core.Cinema;
+import coco.coconutreserve.assets.core.Constants;
+import coco.coconutreserve.assets.core.Films;
+import coco.coconutreserve.assets.core.Init;
 
 public class Dashboard extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Init init = new Init(Constants.CINEMA);
+
+        Cinema[] cinemas = (Cinema[]) init.getData();
+
+        ListView listView = (ListView) findViewById(R.id.activity_list_of_films);
+        FilmAdaptor filmAdaptor = new FilmAdaptor(this, Films.films);
+        listView.setAdapter(filmAdaptor);
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -30,15 +54,5 @@ public class Dashboard extends AppCompatActivity {
             return false;
         }
     };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
 }
--- app/src/main/java/coco/coconutreserve/MainActivity.java
+++ app/src/main/java/coco/coconutreserve/MainActivity.java
@@ -1,8 +1,15 @@
 package coco.coconutreserve;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import coco.coconutreserve.assets.core.Cinema;
 import coco.coconutreserve.assets.core.Constants;
@@ -11,20 +18,44 @@ import coco.coconutreserve.assets.core.Init;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView welcomeMessage;
+    EditText email;
+    EditText password;
+    Button signIn;
+    Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_main);
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+        welcomeMessage = (TextView) findViewById(R.id.welcomeText);
+        email = (EditText) findViewById(R.id.emailText);
+        password = (EditText) findViewById(R.id.passwordText);
+        signIn = (Button) findViewById(R.id.signIn);
+        signUp = (Button) findViewById(R.id.signUp);
+
+        signIn.setOnClickListener( signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText( getApplicationContext(),"User SIGNED IN ",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                startActivity(intent);
+            }
+        }
+        ) );
+
+        signUp.setOnClickListener( signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText( getApplicationContext(),"User SIGNED UP ",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                startActivity(intent);
+            }
+        }));
 
 
-        Init init = new Init(Constants.CINEMA);
-
-        Cinema[] cinemas = (Cinema[]) init.getData();
-
-        ListView listView = (ListView) findViewById(R.id.activity_list_of_films);
-        FilmAdaptor filmAdaptor = new FilmAdaptor(this, Films.films);
-        listView.setAdapter(filmAdaptor);
 
     }
 }
