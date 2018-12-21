a306d6af3cf6ac4bf8bef7a3e486720da18485b4
Nursena Kal <nursena.kal@ug.bilkent.edu.tr>
2018-12-22 01:37:44 +0300

    n

--- app/src/main/java/coco/coconutreserve/MainActivity.java
+++ app/src/main/java/coco/coconutreserve/MainActivity.java
@@ -1,21 +1,14 @@
 package coco.coconutreserve;
 
 import android.content.Intent;
-import android.support.constraint.ConstraintLayout;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import coco.coconutreserve.assets.core.Cinema;
-import coco.coconutreserve.assets.core.Constants;
-import coco.coconutreserve.assets.core.Films;
-import coco.coconutreserve.assets.core.Init;
-
 
 public class MainActivity extends AppCompatActivity {
     TextView welcomeMessage;
@@ -27,25 +20,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+
         welcomeMessage = (TextView) findViewById(R.id.welcomeText);
         email = (EditText) findViewById(R.id.emailText);
         password = (EditText) findViewById(R.id.passwordText);
         signIn = (Button) findViewById(R.id.signIn);
         signUp = (Button) findViewById(R.id.signUp);
 
-        signIn.setOnClickListener( signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText( getApplicationContext(),"User SIGNED IN ",
-                        Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(), Dashboard.class);
-                startActivity(intent);
-            }
-        }
-        ) );
-
-        signUp.setOnClickListener( signUp.setOnClickListener(new View.OnClickListener() {
+        signIn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Toast.makeText( getApplicationContext(),"User SIGNED UP ",
@@ -53,7 +35,16 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), Dashboard.class);
                 startActivity(intent);
             }
-        }));
+        });
+        signUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText( getApplicationContext(),"User SIGNED UP ",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                startActivity(intent);
+            }
+        });
 
 
 
