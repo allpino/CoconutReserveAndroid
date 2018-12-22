7799fe2ef931eb465cb66b53e2624aa12011677c
Nursena Kal <nursena.kal@ug.bilkent.edu.tr>
2018-12-22 17:53:56 +0300

    pushpush baby

---
+++ app/src/main/java/coco/coconutreserve/AccountSettings.java
@@ -0,0 +1,22 @@
+package coco.coconutreserve;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class AccountSettings extends AppCompatActivity {
+
+    TextView aacountSettings , userNameTV ;
+    EditText userNameET ;
+    Switch premiumSwitch;
+    Button walletSetings, saveAndExit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_settings);
+    }
+}
