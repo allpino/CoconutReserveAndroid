ac6e3e067b5d3e4d893084245995d1272ad52e21
Nursena Kal <nursena.kal@ug.bilkent.edu.tr>
2018-12-23 03:25:28 +0300

    heylo

--- app/src/main/java/coco/coconutreserve/Notifications.java
+++ app/src/main/java/coco/coconutreserve/Notifications.java
@@ -17,6 +17,7 @@ public class Notifications extends AppCompatActivity {
         setContentView(R.layout.activity_notifications);
 
 //// ON THIS PAGE, DISCOUNTS WILL BE SHOWN.
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
--- app/src/main/java/coco/coconutreserve/WalletPayment.java
+++ app/src/main/java/coco/coconutreserve/WalletPayment.java
@@ -1,15 +1,63 @@
 package coco.coconutreserve;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import coco.coconutreserve.assets.core.BuyComponent;
+import coco.coconutreserve.assets.core.Constants;
+import coco.coconutreserve.assets.core.Reservation;
+import coco.coconutreserve.assets.core.Wallet;
 
 // pay from wallet
 
 public class WalletPayment extends AppCompatActivity {
-
+    TextView walletName, paymentNumeric, payment, balance, currentWalletInfo, walletBalance, walletNameInfo;
+    Button pay;
+    Wallet wallet;
+    int currentPayment;
+    BuyComponent buyComponent;
+    Reservation reservation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet_payment);
+        walletName = findViewById(R.id.walletName);
+        paymentNumeric = findViewById(R.id.paymentNumeric);
+        payment = findViewById(R.id.payment);
+        balance = findViewById(R.id.balance);
+        currentWalletInfo = findViewById(R.id.currentWalletInfo);
+        walletBalance = findViewById(R.id.walletBalance);
+        walletNameInfo = findViewById(R.id.walletNameInfo);
+        pay = findViewById(R.id.pay);
+        // user wallet info.
+        wallet = new Wallet(100, Constants.CINEMA);
+        currentPayment = 25;
+        buyComponent = new BuyComponent();
+        // instance of wallet is created.
+        //GET BUY COMPONENT WORK PROPERLY.
+
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentPayment > wallet.getAmount())
+                {
+                    Toast.makeText( getApplicationContext()," Your wallet does not have enough money. ",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    // BUY COMPONENT, BUY WITH WALLET.
+
+                    Intent intent1 = new Intent(WalletPayment.this, PaymentSuccess.class);
+                    startActivity(intent1);
+                }
+
+
+            }
+        });
     }
 }
